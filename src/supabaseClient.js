0223d2d7114e2ed18be3f9023422f486edc08f22
TuhakarainaB <46430584+TuhakarainaB@users.noreply.github.com>
2025-04-30 16:29:24 +1200

    Update supabaseClient.js
    Replace URL and Key with generic message

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://moanveycsbptqwnecodb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYW52ZXljc2JwdHF3bmVjb2RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NzM5MzYsImV4cCI6MjA2MTQ0OTkzNn0.A56pAyvfHvtWLlZFf-GZw3fKDyt5gbfjDk3mCG-vQsU';
+const supabaseUrl = 'https://your-project-id.supabase.co';
+const supabaseKey = 'your-anon-key';
 export const supabase = createClient(supabaseUrl, supabaseKey);
